fbc99d1ec821d16a314243755eb0f456b2a7561e
Artur Jedrzejczak <artur.jedrzejczak@pomeranianstartit.pl>
2023-11-18 21:06:14 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Vite + React + TypeScript + Redux + React Router + useParams</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
